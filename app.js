e076c6240c262b556672444612826ca8381641c9
Harishan Thilakanathan <79531856+HariT10@users.noreply.github.com>
2025-05-21 22:01:38 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let players = [];
+
+// Load CSV file
+async function loadCSV() {
+
+    try{
+
+        const fileName = "MLS STATS - Sheet1.csv"
+
+
+
+    } catch(error){
+
+    }
+
+    
+
+
+    
+}
---
+++ parse.py
@@ -0,0 +1,8 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import csv
+import time
+
+
+
